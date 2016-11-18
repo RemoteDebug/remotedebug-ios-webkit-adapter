4b0675763c1eab9be7ca9158947f4a6c68f38462
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-11-18 08:31:39 +0800

    Change index script to work as CLI.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-//
-// Copyright (C) Microsoft. All rights reserved.
-//
+#!/usr/bin/env node
 
 import { ProxyServer } from './server';
+var optimist = require('optimist');
+
+process.title = 'remotedebug-ios-webkit-adapter'
+
+var argv = optimist
+  .usage('Usage: [options]')
+  .alias('p', 'port').describe('p', 'the adapter listerning post').default('p', 9000)
+  .describe('version', 'prints current version').boolean('boolean')
+  .argv
+
+if (argv.version) {
+  console.error(require('./package').version);
+  process.exit(0);
+}
 
 const server = new ProxyServer();
-const port = server.run(9000);
+const port = server.run(argv.port);
 
-console.log(`Proxy server listening on port ${port}`);
+console.log(`remotedebug-ios-webkit-adapter is listening on port ${port}`);
+
+process.on('SIGINT', function () {
+  server.stop();
+  process.exit();
+})
+
+
+
+
