3c85910632b5d62d9d522b0c2b1ddc33a3364ba9
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-03-12 16:07:43 -0700

    Update build script

--- gulpfile.js
+++ gulpfile.js
@@ -2,79 +2,79 @@
 // Copyright (C) Microsoft. All rights reserved.
 //
 
-'use strict';
+'use strict'
 
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-const sourcemaps = require('gulp-sourcemaps');
-const tslint = require('gulp-tslint');
-const ts = require('gulp-typescript');
-const log = require('gulp-util').log;
-const typescript = require('typescript');
-const fs = require('fs');
-const path = require('path');
+const gulp = require('gulp')
+const mocha = require('gulp-mocha')
+const sourcemaps = require('gulp-sourcemaps')
+const tslint = require('gulp-tslint')
+const ts = require('gulp-typescript')
+const log = require('gulp-util').log
+const typescript = require('typescript')
+const fs = require('fs')
+const path = require('path')
 
 const shellSources = [
-    'src/**/*',
-    'test/**/*.ts',
-    'test/*.ts',
-    'typings/globals/**/*.ts'
-];
+  'src/**/*',
+  'test/**/*.ts',
+  'test/*.ts',
+  'typings/globals/**/*.ts',
+  '!src/**/*.json'
+]
 
 const lintSources = [
-    'src/**/*.ts',
-    'test/**/*.ts'
-];
+  'src/**/*.ts',
+  'test/**/*.ts'
+]
 
-let isWatch = false;
+let isWatch = false
 
 gulp.task('build', function () {
-    const tsConfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
-    const projectConfig = tsConfig.compilerOptions;
-    return gulp.src(shellSources, { base: '' })
-        .pipe(sourcemaps.init())
-        .pipe(ts(projectConfig)).js
-        .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: __dirname + '/'}))
-        .pipe(gulp.dest('./out'));
-});
+  var tsProject = ts.createProject('tsconfig.json')
+
+  var tsResult = gulp.src(shellSources, { base: '' })
+    .pipe(tsProject())
+
+  tsResult.pipe(gulp.dest('./out'))
+})
 
 gulp.task('build-tests', function () {
-    const sources = [
-        'test/**/*.ts',
-        'test/*.ts',
-    ];
+  const sources = [
+    'test/**/*.ts',
+    'test/*.ts'
+  ]
 
-    const tsConfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
-    const projectConfig = tsConfig.compilerOptions;
-    return gulp.src(sources, { base: '' })
-        .pipe(sourcemaps.init())
-        .pipe(ts(projectConfig)).js
-        .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: __dirname + '/'}))
-        .pipe(gulp.dest('./out/test'));
-});
+  const tsConfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'))
+  const projectConfig = tsConfig.compilerOptions
+  return gulp.src(sources, { base: '' })
+    .pipe(sourcemaps.init())
+    .pipe(ts(projectConfig)).js
+    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: __dirname + '/'}))
+    .pipe(gulp.dest('./out/test'))
+})
 
 gulp.task('lint', function () {
-    console.log(lintSources)
-    return gulp.src(lintSources)
-        .pipe(tslint({formatter: "full"}))
-        .pipe(tslint.report('verbose'));
-});
+  console.log(lintSources)
+  return gulp.src(lintSources)
+    .pipe(tslint({formatter: 'full'}))
+    .pipe(tslint.report('verbose'))
+})
 
-gulp.task('test', ['build-tests'], function() {
-    process.env.NODE_ENV = 'development';
-    return gulp.src('out/test/**/*.test.js', { read: false })
-        .pipe(mocha({ ui: 'tdd' }))
-        .on('error', function(e) {
-            log(e ? e.toString() : 'error in test task!');
-            this.emit('end');
-        });
-});
+gulp.task('test', ['build-tests'], function () {
+  process.env.NODE_ENV = 'development'
+  return gulp.src('out/test/**/*.test.js', { read: false })
+    .pipe(mocha({ ui: 'tdd' }))
+    .on('error', function (e) {
+      log(e ? e.toString() : 'error in test task!')
+      this.emit('end')
+    })
+})
 
-gulp.task('watch-test', ['build-tests'], function() {
-     return gulp.watch(shellSources, ['build-tests']);
-});
+gulp.task('watch-test', ['build-tests'], function () {
+  return gulp.watch(shellSources, ['build-tests'])
+})
 
 gulp.task('watch', ['build'], function () {
-    const all = shellSources;
-    gulp.watch(all, ['build']);
-});
+  const all = shellSources
+  gulp.watch(all, ['build'])
+})
