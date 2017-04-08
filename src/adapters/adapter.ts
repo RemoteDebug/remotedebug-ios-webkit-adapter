e48496715045900b2d0a9f1ae054f436975c08ad
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-04-08 09:52:43 -0700

    Update Chrome DevTools front-end

--- src/adapters/adapter.ts
+++ src/adapters/adapter.ts
@@ -151,7 +151,7 @@ export class Adapter extends EventEmitter {
         // Overwrite the real endpoint with the url of our proxy multiplexor
         t.webSocketDebuggerUrl = `${this._proxyUrl}${this._id}/${t.id}`;
         let wsUrl = `${this._proxyUrl.replace('ws://', '')}${this._id}/${t.id}`;
-        t.devtoolsFrontendUrl = `https://chrome-devtools-frontend.appspot.com/serve_file/@60cd6e859b9f557d2312f5bf532f6aec5f284980/inspector.html?experiments=true&remoteFrontend=screencast&ws=${wsUrl}`;
+        t.devtoolsFrontendUrl = `https://chrome-devtools-frontend.appspot.com/serve_file/@7d149ef5473e980f0b3babd4d0f2839cb9338e73/inspector.html?experiments=true&remoteFrontend=screencast&ws=${wsUrl}`;
 
         return t;
     }
